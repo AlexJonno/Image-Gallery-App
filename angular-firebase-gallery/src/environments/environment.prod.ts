0a4c7bafbca61103e8aca952acf46ef959823b4c
AlexJonno <alex.johnston@rocketmail.com>
2017-09-25 15:46:23 +0100

    version1

---
+++ angular-firebase-gallery/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC3HMDv1uI20VShD7428MU7N_cCkGE6OKk",
+    authDomain: "angular-gallery-60757.firebaseapp.com",
+    databaseURL: "https://angular-gallery-60757.firebaseio.com",
+    projectId: "angular-gallery-60757",
+    storageBucket: "angular-gallery-60757.appspot.com",
+    messagingSenderId: "1086427991047"
+  }
+};
---
+++ angular-firebase-gallery/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC3HMDv1uI20VShD7428MU7N_cCkGE6OKk",
+    authDomain: "angular-gallery-60757.firebaseapp.com",
+    databaseURL: "https://angular-gallery-60757.firebaseio.com",
+    projectId: "angular-gallery-60757",
+    storageBucket: "angular-gallery-60757.appspot.com",
+    messagingSenderId: "1086427991047"
+  }
+};
